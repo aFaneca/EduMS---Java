9db1d74d516e900c3fe34b1cca1a85eef5a6e9b2
António Faneca <me@afaneca.com>
2017-11-29 17:28:43 +0000

    Creation of Constructor of Class.

--- EduMS/src/edums/Student.java
+++ EduMS/src/edums/Student.java
@@ -4,11 +4,26 @@
  * and open the template in the editor.
  */
 package edums;
-
+import java.util.*;
 /**
  *
  * @author Diogo
  */
 public class Student {
+ 
+
+        
     
+    public Student(){
+        
+    }
+    
+    
+    
+   @Override
+   public String toString(){
+        String str;
+       
+       return str;
+    }
 }
